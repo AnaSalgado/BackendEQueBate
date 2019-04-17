d971dd5efab96c3884b24872bcdf11154b30dc37
themiranda15 <43752934+themiranda15@users.noreply.github.com>
2019-04-17 16:11:54 +0100

    Add files via upload

---
+++ Vacina.java
@@ -0,0 +1,28 @@
+
+public class Vacina {
+	
+	public String id_vacina;
+
+	public String nome_vac;
+
+	public String descricao_vac;
+
+	public String data_validade;
+
+	public String id_bol;
+	
+	
+	public Vacina (String  id_vacina, String nome_vac, String descricao_vac, String data_validade, String id_bol)
+	{
+		this. id_vacina =  id_vacina;
+	
+		this.nome_vac = nome_vac;
+	
+		this.descricao_vac = descricao_vac;
+	
+		this.data_validade = data_validade;
+		
+		this.id_bol = id_bol;
+	
+}
+}
---
+++ Veterinario.java
@@ -0,0 +1,42 @@
+
+public class Veterinario {
+	
+	public String id_vet;
+
+	public String nome_vet;
+
+	public String data_nasc_vet;
+
+	public String pass_vet;
+
+	public String telemovel_vet;
+	
+	public String morada_vet;
+	
+	public String email_vet;
+	 
+	public String id_seg;
+	
+
+	
+	public Veterinario (String  id_vet, String nome_vet, String data_nasc_vet, String pass_vet, String telemovel_vet, String morada_vet, String email_vet, String id_seg)
+	{
+		this.id_vet =  id_vet;
+	
+		this.nome_vet= nome_vet;
+	
+		this.data_nasc_vet = data_nasc_vet;
+	
+		this.pass_vet = pass_vet;
+		
+		this.telemovel_vet = telemovel_vet;
+		
+		this.morada_vet = morada_vet;
+		
+		this.email_vet = email_vet;
+		
+		this.id_seg = id_seg;
+		
+	}
+	
+}
