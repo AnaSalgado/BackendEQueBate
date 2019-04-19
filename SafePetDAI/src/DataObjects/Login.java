cab69fe9c42c76dde86f9df18032c845f1e22bee
claudiasilva99 <43752879+claudiasilva99@users.noreply.github.com>
2019-04-19 23:04:49 +0100

    Add files via upload

--- SafePetDAI/src/DataObjects/Login.java
+++ SafePetDAI/src/DataObjects/Login.java
@@ -1,21 +1,30 @@
 package DataObjects;
 
+import java.util.ArrayList;
 
-
-public boolean class Login (String email, String password){
-
-	for(User user: this.user_list) {
-		 if(user.getEmailUser().equals(email) && user.getPassUser().equals(password)){
-			user = user;
-			return true;
-		 }
+public class Login{
+	
+	public String password;
+	public String email;
+	public ArrayList<User> user_list = new ArrayList<User>();
+	
+	public Login (String email, String password) {
+		this.email=email;
+		this.password=password;
+		user_list = new ArrayList<User>();
 	}
-	return false;
+	
+	
+	public boolean LoginUser(String email, String password) {
+
+		boolean exis = false;
+		for(User user: user_list) {
+			if(user.getEmailUser().equals(email) && user.getPassUser().equals(password)){
+				exis = true;
+				} else {
+					exis = false;
+					}
+			}
+		return exis;
+		}
 	}
-
-
-
-
-
-
-
