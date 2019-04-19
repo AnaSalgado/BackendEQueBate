2d47c2e1235fbd8e8e24f6b24204b7117c8b720e
themiranda15 <43752934+themiranda15@users.noreply.github.com>
2019-04-19 10:45:52 +0100

    Add files via upload

---
+++ SafePetDAI/src/DataObjects/User.java
@@ -0,0 +1,31 @@
+package DataObjects;
+
+
+public class User {
+	public String email_user;
+
+	public String pass_user;
+
+	
+	
+	
+	public User (String  email_user, String pass_user)
+	{
+		this.email_user = email_user;
+	
+		this.pass_user = pass_user;
+	
+}
+	
+	public String getEmailUser() {
+		return email_user;
+		}
+
+	public String getPassUser() {
+		return pass_user;
+	}
+
+	
+
+
+}
