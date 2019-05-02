38e4c8f8e8799c902284d663790865f63f8941e6
claudiasilva99 <43752879+claudiasilva99@users.noreply.github.com>
2019-05-02 20:47:43 +0100

    Add files via upload

--- SafePetDAI/src/Login.java
+++ SafePetDAI/src/Login.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login
  */
-@WebServlet (urlPatterns = {"/loginowner/*", "/logout", "/loginvet/*", "/loginins/*"})
+@WebServlet (urlPatterns = {"/login/*"})
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -54,42 +54,49 @@ public class Login extends HttpServlet {
 		    String wheremail = "";
 		    String wherepass = "";
 		    System.out.println(email + "\n" + pass);
-
 		    switch(request.getRequestURI()) {
-		    case "/SafePetDAI/loginowner":
+		    case "/SafePetDAI/login":
+		    table = request.getParameter("table");
+		    System.out.println(table);
+		    
+		    if (table!=null && table.equals("Donos")) {
 		    	 String emaild = request.getParameter("email");
 		    	 String passd = request.getParameter("pass");
 		    	 email=emaild;
 		    	 pass=passd;
-		    	 table = "Donos";
+		    	// table = "Donos";
 		    	 wheremail = "email_dono";
 		    	 wherepass = "password_dono";
 		    	 System.out.println(emaild + "\n" + passd);
-		   break;
-		    
-		    case "/SafePetDAI/loginvet":
-		    	String emailv = request.getParameter("email");
-		    	String passv = request.getParameter("pass");
-		    	email=emailv;
-		    	pass=passv;
-		    	table = "Veterinarios";
-		    	wheremail = "email_vet";
-		    	wherepass = "pass_vet";
-		    	System.out.println(emailv + "\n" + passv);
-		    	break;
-		    	
-		    case "/SafePetDAI/loginins":
+		    }else {
+		    if (table!=null && table.equals("Seguradoras")) {
 		    	String emaili = request.getParameter("email");
 		    	String passi = request.getParameter("pass");
 		    	email=emaili;
 		    	pass=passi;
-		    	table = "Seguradoras";
+		    	//table = "Seguradoras";
 		    	wheremail = "email_seg";
 		    	wherepass = "pass_seg";
 		    	System.out.println(emaili + "\n" + passi);
-		    	break;
+		    }else {
+		    if (table!=null && table.equals("Veterinarios")) {
+		    	String emailv = request.getParameter("email");
+		    	String passv = request.getParameter("pass");
+		    	email=emailv;
+		    	pass=passv;
+		    	//table = "Veterinarios";
+		    	wheremail = "email_vet";
+		    	wherepass = "pass_vet";
+		    	System.out.println(emailv + "\n" + passv);
+		    } else {
+		    	System.out.println("Table incorreta");
+		    }
+		    }
+		    }
+		    break;
 		    }
 		    
+		    System.out.println(email +"\n" + pass + "\n" + table + "\n" + wheremail + "\n" + wherepass);
 		    try {
 				ConnectionDB.checkUser(email, pass, table, wheremail, wherepass);
 			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
