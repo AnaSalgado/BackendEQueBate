83691f8d384339437071936ffa0c2d482b733aed
themiranda15 <43752934+themiranda15@users.noreply.github.com>
2019-05-18 19:07:32 +0100

    Add files via upload

---
+++ SafePetDAI/src/MyServlet1.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.http.*;
+
+/**
+ * Servlet implementation class MyServlet1
+ */
+@WebServlet("/MyServlet1")
+public class MyServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		 try{
+		      response.setContentType("text/html");
+		      PrintWriter pwriter = response.getWriter();
+
+		      String email = request.getParameter("email");
+		      String password = request.getParameter("password");
+		      
+		      HttpSession session=request.getSession();
+		      session.setAttribute("uemail",email);
+		      session.setAttribute("upass",password);
+		     // pwriter.print("<a href='welcome'>view details</a>");
+		      pwriter.close();
+		    }catch(Exception exp){
+		       System.out.println(exp);
+		     }
+		  }
+	
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SafePetDAI/src/MyServlet2.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import javax.servlet.http.*;
+/**
+ * Servlet implementation class MyServlet2
+ */
+@WebServlet("/MyServlet2")
+public class MyServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		try{
+		      response.setContentType("text/html");
+		      PrintWriter pwriter = response.getWriter();
+		      HttpSession session=request.getSession(false);
+		      String myEmail=(String)session.getAttribute("uemail");
+		      String myPass=(String)session.getAttribute("upass");
+		      pwriter.print("Email: "+myEmail+" Pass: "+myPass);
+		      pwriter.close();
+		  }catch(Exception exp){
+		      System.out.println(exp);
+		   }
+		  }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
